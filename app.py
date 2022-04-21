3d44fac7a20d66aec104c68e9e8c4a37c719ff31
GnSit <rrdgnm@yahoo.com>
2022-04-22 01:41:51 +0300

    apiip

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from koulu import app
 
 if __name__ == "__main__":
-    # from koulu import db
-    # from koulu.logic.models import Members, Viesti
+    from koulu import db
+    from koulu.logic.models import Members, Viesti
     # db.drop_all()
-    # db.create_all()
+    db.create_all()
     app.run()
